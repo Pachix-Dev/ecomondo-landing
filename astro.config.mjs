0a7d1a456644a3f0260bcd0cb5c6172d099b3678
Pachi-Dev <pachi.claros@gmail.com>
2024-01-11 10:25:20 -0600

    Update Astro configuration and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
+  site:'https://ecomondo.igeco.mx',
   integrations: [tailwind(), react(), sitemap()]
 });
